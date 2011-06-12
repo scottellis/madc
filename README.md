acd88091df6e284a60c417bced63a3f43ca797e0
Scott Ellis <scott@jumpnowtek.com>
2011-06-12 11:42:19 -0400

    Added averaging and a machine friendly format

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Simple example program to read the Overo twl4030-madc values.
 The twl4030-madc are 10-bit ADC's with a voltage input range 0 - 2.5v
 
 
-  Download Code
+  Download and Build
 -------
 
 The Makefile assumes a typical Gumstix OE cross-compile setup on a workstation.
@@ -19,39 +19,44 @@ The Makefile assumes a typical Gumstix OE cross-compile setup on a workstation.
 Copy the resulting madc-test executable to the Overo however is convenient.
 
 
-  Run
+  Usage
 -------
 
 Assuming you copied the madc-test program to /home/root on the Overo.
 
-	root@overo:~# ./madc-test all
-	madc: ch =  0 status = 0  result =    0  voltage = 0.00
-	madc: ch =  1 status = 0  result =   50  voltage = 0.12
-	madc: ch =  2 status = 0  result =    6  voltage = 0.01
-	madc: ch =  3 status = 0  result =    3  voltage = 0.01
-	madc: ch =  4 status = 0  result =    5  voltage = 0.01
-	madc: ch =  5 status = 0  result =    3  voltage = 0.01
-	madc: ch =  6 status = 0  result =    3  voltage = 0.01
-	madc: ch =  7 status = 0  result =  560  voltage = 1.37
-	madc: ch =  8 status = 0  result =    4  voltage = 0.01
-	madc: ch =  9 status = 0  result =  719  voltage = 1.76
-	madc: ch = 10 status = 0  result =    0  voltage = 0.00
-	madc: ch = 11 status = 0  result =    0  voltage = 0.00
-	madc: ch = 12 status = 0  result =  565  voltage = 1.38
-	madc: ch = 13 status = 0  result =    4  voltage = 0.01
-	madc: ch = 14 status = 0  result =    2  voltage = 0.00
-	madc: ch = 15 status = 0  result =    0  voltage = 0.00
+	root@overo:~# ./madc-test -h
+	Usage: ./madc-test [options] [channel list]
+	Options
+	  -a          Request the device driver average 4 readings
+	  -m          Machine readable format ch:raw:voltage
+	  -h          Show this help message
 
-To read specific channels only, pass the channels, space separated
+	channel list  A space separated list of channel numbers 0-15.
+		      If no channel list is supplied, channels 2-6 are read.
 
-	root@overo:~# ./madc-test 2 3 4 5 6
-	madc: ch =  2 status = 0  result =  644  voltage = 1.57
-	madc: ch =  3 status = 0  result =  646  voltage = 1.58
-	madc: ch =  4 status = 0  result =  643  voltage = 1.57
-	madc: ch =  5 status = 0  result =  647  voltage = 1.58
-	madc: ch =  6 status = 0  result =  646  voltage = 1.58
+	  Example: ./madc-test -a 2 4 6
 
-(Channels 2-6 were exposed to a 1.5v signal for that test.)
+
+So for example
+
+	root@overo:~# ./madc-test   
+	madc[ 2] raw =  413  voltage = 1.01
+	madc[ 3] raw =  405  voltage = 0.99
+	madc[ 4] raw =  407  voltage = 0.99
+	madc[ 5] raw =  405  voltage = 0.99
+	madc[ 6] raw =  403  voltage = 0.98
+
+Channels 2-6 had 1.0v applied with a bench power supply.
+
+
+Or two channels, average of 4 reads, output in machine friendly format 
+
+	root@overo:~# ./madc-test -a -m 2 4   
+	2:615:1.50
+	4:618:1.51
+
+Channel 2 and 4 had ~1.5v applied for that test.
+
 
 The exposed ADC pins on the Overo Summit/Tobi expansion headers.
 
@@ -62,6 +67,6 @@ The exposed ADC pins on the Overo Summit/Tobi expansion headers.
 	ADCIN4 : Pin 39
 	ADCIN5 : Pin 36
 	ADCIN6 : Pin 35
-	ADCIN7 : Pin 33  appears to be in use, TODO
+	ADCIN7 : Pin 33  appears to be in use, TODO: research this
 
 
--- madc.c
+++ madc.c
@@ -19,8 +19,12 @@ typedef uint16_t u16;
  
 #include "twl4030-madc.h"
 
+/* only query the external channels 2-7 when 'all' selected */
+#define MIN_CHANNEL 2
+#define MAX_CHANNEL 7
 
-int read_channel(int fd, int ch)
+
+int read_channel(int fd, int ch, int avg, int machine_read)
 {
 	int ret;
 	struct twl4030_madc_user_parms param;
@@ -29,6 +33,7 @@ int read_channel(int fd, int ch)
 	memset(&param, 0, sizeof(param));
 
 	param.channel = ch;
+	param.average = avg;
 
 	ret = ioctl(fd, TWL4030_MADC_IOCX_ADC_RAW_READ, &param);
 
@@ -37,28 +42,49 @@ int read_channel(int fd, int ch)
 		return ret;
 	}
 
-	/* 10 bit ADC, reference voltage 2.5v */
-	voltage = (param.result * 2.5) / 1024.0;
+	if (param.status == -1) {
+		if (machine_read)
+			printf("%d:-1:-1", ch);
+		else
+			printf("madc[%d]: status = -1\n", ch);
+	}
+	else {
+		/* 10 bit ADC, reference voltage 2.5v */
+		voltage = (param.result * 2.5) / 1024.0;
 
-	printf("madc: ch = %2d status = %d  result = %4u  voltage = %0.2lf\n",
-		param.channel, param.status, param.result, voltage);
+		if (machine_read)
+			printf("%d:%u:%0.2lf\n", ch, param.result, voltage);
+		else
+			printf("madc[%2d] raw = %4u  voltage = %0.2lf\n",
+				ch, param.result, voltage);
+	}
 
 	return param.status;
 }
 
-void read_all_channels(int fd)
+void read_all_channels(int fd, int avg, int machine_read)
 {
 	int i;
 
-	for (i = 0; i < TWL4030_MADC_MAX_CHANNELS; i++)
-		read_channel(fd, i);
+	for (i = MIN_CHANNEL; i < MAX_CHANNEL; i++)
+		read_channel(fd, i, avg, machine_read);
+}
+
+void usage(const char *argv_0)
+{
+	printf("Usage: %s [options] [channel list]\n", argv_0);
+	printf("Options\n");
+	printf("  -a          Request the device driver average 4 readings\n");
+	printf("  -m          Machine readable format ch:raw:voltage\n");
+	printf("  -h          Show this help message\n\n");
+	printf("channel list  A space separated list of channel numbers 0-15.\n");
+	printf("              If no channel list is supplied, channels 2-6 are read.\n\n");
+	printf("  Example: %s -a 2 4 6\n\n", argv_0);
 }
 
 int main(int argc, char **argv)
 {
-	int fd, i, ch, done;
-
-	done = 0;
+	int fd, i, ch, avg, machine_read, opt;
 
 	fd = open("/dev/twl4030-madc", O_RDWR | O_NONBLOCK, 0);
 	if (fd < 0) {
@@ -66,27 +92,42 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	if (argc < 2) {
-		printf("Usage: %s <channel list, space separated>\n", argv[0]);
-		printf("       %s all\n", argv[0]);
-		exit(0);
-	}
-	else if (argc == 2) {
-		/* special case, check for 'all' */
-		if (!strcmp(argv[1], "all")) {
-			read_all_channels(fd);
-			done = 1;
-		}
-	}
+	ch = -1;
+	avg = 0;
+	machine_read = 0;
 
-	if (!done) {
-		for (i = 1; i < argc; i++) {
-			ch = atoi(argv[i]);	
-				
-			if (ch >= 0 && ch < TWL4030_MADC_MAX_CHANNELS)
-				read_channel(fd, ch);
+	while ((opt = getopt(argc, argv, "amh")) != -1) {
+		switch (opt) {
+		case 'a':
+			avg = 1;
+			break;
+
+		case 'm':
+			machine_read = 1;
+			break;
+
+		case 'h':
+			usage(argv[0]);
+			exit(0);
+
+		default:
+			printf("Unknown arg: %s\n", optarg);
+			usage(argv[0]);
+			exit(1);
+		}
+	}	
+
+	if (optind == argc) {
+		read_all_channels(fd, avg, machine_read);
+	}
+	else {
+		for (i = optind; i < argc; i++) {
+			ch = atoi(argv[i]);
+
+			if (ch < 0 || ch >= TWL4030_MADC_MAX_CHANNELS)
+				printf("Invalid channel arg: %s\n", argv[i]);
 			else
-				printf("Bad channel number %d\n", ch);
+				read_channel(fd, ch, avg, machine_read); 
 		}
 	}
 
